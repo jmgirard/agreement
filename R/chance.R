d89a9db85c58cff155fb6f0699f68a288ba7e5d9
Jeffrey Girard <me@jmgirard.com>
2018-08-29 10:10:45 -0400

    Play with variable naming

--- R/chance.R
+++ R/chance.R
@@ -1,14 +1,17 @@
 chance_ckappa <- function(codes, categories, weights) {
 
   # Get basic counts
-  n_objects <- nrow(codes)
-  n_raters <- ncol(codes)
-  n_categories <- length(categories)
+  n_o <- nrow(codes) # objects
+  n_r <- ncol(codes) # raters
+  n_c <- length(categories) # categories
 
-  # Create objects (i.e., rater-by-category) matrix
-  mat_objects <- matrix(0, nrow = n_raters, ncol = n_categories)
+  # Create object-counts in rater-by-category matrix
+  mat_o_rc <- matrix(0, nrow = n_r, ncol = n_c)
   for (k in seq_along(categories)) {
-    mat_objects[, k] <- rowSums(codes == categories[[k]], na.rm = TRUE)
+    mat_o_rc[, k] <- colSums(codes == categories[[k]], na.rm = TRUE)
   }
 
+  sum_o_r <- rowSums(rxc, na.rm = TRUE)
+  mat_rc_max <- sum_o_r %*% matrix(1, nrow = 1, ncol = n_c)
+  mat_ <- mat_objects / mat_counts
 }
