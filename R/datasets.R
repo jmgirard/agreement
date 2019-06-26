92653dad6c1d22be71424284802b8da133ab66b6
jmgirard <me@jmgirard.com>
2019-06-26 00:23:51 -0400

    Add gwet dataset

--- R/datasets.R
+++ R/datasets.R
@@ -3,3 +3,10 @@
 #' Example categorical data describing 4 fish coded by 5 raters.
 #'
 "fish"
+
+#' Interval example data
+#'
+#' Example data with 16 objects rated by 4 raters.
+#'
+#' @references Gwet (2014), Exercise 3.3
+"gwet3.3"
