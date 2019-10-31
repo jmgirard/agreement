ba7270321df8088423a84ec309170762ae86571b
Jeffrey Girard <me@jmgirard.com>
2019-10-31 18:24:41 -0400

    Import as_tibble

--- R/agreement-package.R
+++ R/agreement-package.R
@@ -2,5 +2,5 @@
 #' @importFrom assertthat assert_that is.count is.flag is.number is.scalar
 #'   is.string
 #' @importFrom rlang are_na is_empty is_na is_null is_vector
-#' @importFrom tibble tibble tribble
+#' @importFrom tibble as_tibble tibble tribble
 "_PACKAGE"
