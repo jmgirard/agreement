8335ca6186aa51a9b19c8ad8a96268a6f91470a0
Jeffrey Girard <me@jmgirard.com>
2019-11-05 18:17:12 -0500

    Add basic documentation

--- R/generic_utilities.R
+++ R/generic_utilities.R
@@ -1,4 +1,12 @@
-# Convert wide format to long format
+#' Convert wide format to long format
+#'
+#' Description
+#'
+#' @param x *Required.* A data frame containing rating data in wide format.
+#' @param ... *Optional.* Additional arguments passed on to
+#'   \code{tidyr::pivot_longer()/}.
+#' @return A tibble containing the data from \code{x} but in long format.
+#' @export
 wide_to_long <- function(x, ...) {
 
   assert_that(is.data.frame(x) || is.matrix(x))
@@ -7,6 +15,7 @@ wide_to_long <- function(x, ...) {
 
   out <-
     x %>%
+    as_tibble() %>%
     dplyr::mutate(Trial = 1, Object = o) %>%
     tidyr::pivot_longer(
       cols = -c(Trial, Object),
@@ -18,13 +27,26 @@ wide_to_long <- function(x, ...) {
   out
 }
 
-# Convert long format to wide format
+#' Convert long format to wide format
+#'
+#' Description
+#'
+#' @param x *Required.* A data frame containing rating data in long format.
+#' @param rater *Required.* The name of the variable in \code{x} that indicates
+#'   the rater for each measurement.
+#' @param score *Required.* The name of the variable in \code{x} that indicates
+#'   the score for each measurement.
+#' @param ... *Optional.* Additional arguments passed on to
+#'   \code{tidyr::pivot_wider()}.
+#' @return A tibble containing the data from \code{x} but in wide format.
+#' @export
 long_to_wide <- function(x, rater, score, ...) {
 
   assert_that(is.data.frame(x) || is.matrix(x))
 
   out <-
     x %>%
+    as_tibble() %>%
     tidyr::pivot_wider(
       names_from = {{rater}},
       values_from = {{score}},
@@ -34,7 +56,16 @@ long_to_wide <- function(x, rater, score, ...) {
   out
 }
 
-# Convert table format to wide format
+#' Convert table format to wide format
+#'
+#' Description
+#'
+#' @param x *Required.* A matrix, dataframe, or table object representing a
+#'   two-by-two contingency matrix (AKA confusion matrix).
+#' @param raters *Optional.* A character vector containing the name of each
+#'   rater.
+#' @return A tibble containing the data from \code{x} in wide format.
+#' @export
 table_to_wide <- function(x, raters = NULL) {
   requireNamespace("DescTools", quietly = TRUE)
   wide <- DescTools::Untable(x)
@@ -43,12 +74,18 @@ table_to_wide <- function(x, raters = NULL) {
   } else {
     colnames(wide) <- raters
   }
-  wide <- tibble::as_tibble(wide)
+  wide <- as_tibble(wide)
 
   wide
 }
 
-# Convert table format to long format
+#' Convert table format to long format
+#'
+#' Description
+#'
+#' @inheritParams table_to_wide
+#' @return A tibble containing the data from \code{x} in long format.
+#' @export
 table_to_long <- function(x, raters = NULL) {
   wide <- table_to_wide(x, raters)
   long <- wide_to_long(wide)
