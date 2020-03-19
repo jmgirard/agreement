102de069e6e7397f8c81778a8a901f65df45660b
Jeffrey Girard <me@jmgirard.com>
2020-03-19 18:03:07 -0400

    Add mcgraw ICC data in wide and long formats

--- data-raw/mcgraw.R
+++ data-raw/mcgraw.R
@@ -1,4 +1,4 @@
-mcgraw <-
+mcgraw_wide <-
   tibble::tribble(
     ~M, ~C,
     103, 119,
@@ -12,5 +12,13 @@ mcgraw <-
     97, 101,
     107, 110
   )
+usethis::use_data(mcgraw_wide, overwrite = TRUE)
 
+
+# -------------------------------------------------------------------------
+
+mcgraw <-
+  mcgraw_wide %>%
+  dplyr::mutate(Object = dplyr::row_number()) %>%
+  tidyr::pivot_longer(cols = M:C, names_to = "Rater", values_to = "Score")
 usethis::use_data(mcgraw, overwrite = TRUE)
