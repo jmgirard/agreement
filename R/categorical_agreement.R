6631c862da5fcc2a12a300c155eae6ff370441f5
jmgirard <me@jmgirard.com>
2020-05-07 16:55:20 -0400

    Fix bug in calculation of Krippendorff's alpha in the presence of missing data

--- R/categorical_agreement.R
+++ R/categorical_agreement.R
@@ -34,13 +34,12 @@ calc_agreement_kripp <- function(codes, categories, weight_matrix) {
   # How many raters assigned each object to any category?
   r_o <- rowSums(r_oc)
 
-  # How much agreement was observed for each object-category combination?
-  obs_oc <- t(weight_matrix %*% t(r_oc))
-
   # Remove objects with only 1 rater
   r_oc <- r_oc[r_o >= 2, ]
-  obs_oc <- obs_oc[r_o >= 2, ]
   r_o <- r_o[r_o >= 2]
+
+  # How much agreement was observed for each object-category combination?
+  obs_oc <- r_oc * t(weight_matrix %*% t(r_oc) - 1)
 
   # What was the average number of raters per object?
   rbar <- mean(r_o)
@@ -49,16 +48,19 @@ calc_agreement_kripp <- function(codes, categories, weight_matrix) {
   nprime <- length(r_o)
 
   # How much agreement was observed for each object across all categories?
-  obs_o <- (r_oc * (obs_oc - 1)) %*% matrix(1, nrow = length(categories))
+  obs_o <- rowSums(obs_oc)
 
-  # How much agreement was possible for each object across all categories?
+  # How much agreement was maximally possible for each object?
   max_o <- rbar * (r_o - 1)
 
-  # Correction factor
-  epsilon <- 1 / sum(r_o) # should this be 1 / (sum(r_o) * nprime)?
+  # What is the standard percent observed agreement?
+  poa_prime <- mean(obs_o / max_o, na.rm = TRUE)
 
-  # What was teh percent observed agreement across all objects?
-  poa <- (1 - epsilon) * sum(obs_o / max_o) / nprime + epsilon
+  # Correction factor
+  epsilon <- 1 / (nprime * rbar)
+
+  # What is the "corrected" percent observed agreement?
+  poa <- (1 - epsilon) * poa_prime + epsilon
 
   poa
 }
